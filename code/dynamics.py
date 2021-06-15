Re-implementation and bug fix of force computation

code/dynamics.py
@@ -23,6 +23,7 @@ class MolecularDynamics:
         self.particlePositions = np.zeros((numberOfParticles, dimension))
         self.particleVelocities = (np.random.rand(numberOfParticles, dimension)-0.5) #assign randomly
         self.particleForces = np.zeros((numberOfParticles, dimension))
+        self.energy = 0
         self.latticePositions()
 
     def latticePositions(self):
@@ -44,50 +45,33 @@ class MolecularDynamics:
         # Ensure that momentum is zero
         v_cm = np.mean(self.particleVelocities, axis=0)
         self.particleVelocities = fs*(self.particleVelocities - v_cm)
-        
-    def evaluateKineticEnergy(self):
-        return 0.5*np.sum(np.square(self.particleVelocities))
-
-    def evaluatePotentialEnergy(self):
-        energy = 0
-        for i in range(self.numberOfParticles):
-            for j in range(i+1, self.numberOfParticles):
-                displacement = self.particlePositions[i,:]-self.particlePositions[j,:]
-                for k in range(self.dimension):
-                    # this ensures the periodicity of the box
-                    if abs(displacement[k])>self.boxLength/2:
-                        displacement[k] -= self.boxLength*np.sign(displacement[k])
-                r = np.linalg.norm(displacement,2)
-                # Using this makes energy fluctutate. 
-                if r > self.rc:
-                    energy += 0 # truncate potential
-                else:    
-                    energy += (4*self.epsilon*((self.sigma/r)**12-(self.sigma/r)**6))
-        return energy
 
     def evaluateTotalEnergy(self):
-        return self.evaluatePotentialEnergy()+self.evaluateKineticEnergy()
+        # Kinetic energy is not required apparently
+        # K = 0.5*np.sum(np.square(self.particleVelocities)) 
+        # return self.PotentialEnergy + K
+        return self.energy
 
     def evaluateForce(self):
         #force = - gradient of potential
+        
         self.particleForces = np.zeros((self.numberOfParticles, self.dimension))
-        for i in range(self.numberOfParticles):
+        for i in range(self.numberOfParticles-1):
             for j in range(i+1, self.numberOfParticles):
-                rij = self.particlePositions[i,:]-self.particlePositions[j,:]
-                for k in range(self.dimension):
-                    # periodicity of the box
-                    if abs(rij[k])>self.boxLength/2:
-                        rij[k] -= self.boxLength*np.sign(rij[k])
-                # rji = -rij
-                self.particleForces[i,:] += self.LJForce(rij)
+                xr = self.particlePositions[i,:]-self.particlePositions[j,:]
+                xr -= self.boxLength*np.ceil(xr/self.boxLength)
+                self.particleForces[i,:] += self.LJForce(xr)
                 self.particleForces[j,:] += -self.particleForces[i,:]
 
     def LJForce(self, xr):
         r = np.linalg.norm(xr, 2)
+        ecut = (4*self.epsilon*((self.sigma/self.rc)**12-(self.sigma/self.rc)**6))
         if r > self.rc:
             force = 0
         else:
-            force = 48/(r**2)*(1/(r**12)-0.5*1/(r**6))*xr
+            force = 48/(r**2)*(1/(r**12)-0.5*1/(r**6))*xr - ecut
+        # Update energy
+        self.energy += 4*(1/r**6)*(1/r**6 - 1) - ecut
         return force
 
     def IntegrateVerlet(self):
@@ -109,10 +93,10 @@ class MolecularDynamics:
         for i in range(self.numberOfParticles):
             file_xyz.write("{} {} \n".format(self.particlePositions[i][0], self.particlePositions[i][1]))
 
-    def plot(self, filename):
+    def plot(self, filename, timestep):
         '''For 2D, I suggest to use the good and old matplotlib'''
         plt.figure()
-        plt.title("System at timestep ...")
+        plt.title("System at timestep {}".format(timestep))
         plt.xlabel("x")
         plt.ylabel("y")
         plt.xlim([-0.1*self.boxLength, 1.05*self.boxLength])