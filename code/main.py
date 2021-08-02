204cceefed518bc8dc16759de996ffd175816e28
rage997 <zuppin@usi.ch>
2021-08-02 15:18:54 +0200

    Refactor and plots

--- code/main.py
+++ code/main.py
@@ -11,22 +11,18 @@ dimension = 2 # this should work also in 3D but haven't tested much. Future work
 sigma = 1
 epsilon = 1
 rc = 2.5
-
 temperature = 0.5
 L = 30
 dt = 0.0001 #time step
-T_max = 25000
+T_max = 2000
 
 #set up the simulation
 simulation = dynamics.MolecularDynamics(numberOfParticles, L, dimension, 
                         sigma, epsilon, rc, temperature, dt)
 
-#calculating forces on particles in the box
-simulation.evaluateForce()
-
 #making a list of particle positions and velocities for acf and what not. You could make an animation out of this
-particlePositionsList = [simulation.particlePositions]
-particleVelocityList = [simulation.particleVelocities]
+particlePositionsList = []
+particleVelocityList = []
 
 #making a list of energies at various time steps to plot later
 energy = np.zeros(T_max+1,)
@@ -34,13 +30,13 @@ energy[0] = simulation.evaluateTotalEnergy()
 momentum = np.zeros(T_max+1,)
 momentum[0] = simulation.evaluateTotalMomentum()
 
-
 for i in range(T_max):
     if i % 1000 == 0:
        print('timestep:', i)
        simulation.plot('system{}.png'.format(i), i)
 
     #do the time step, knowing that simulation already knows the particle forces at the moment
+    simulation.evaluateForce()
     simulation.IntegrateVerlet() #evaluates forces on particles, updates particle positions and velocities
     energy[i+1] = simulation.evaluateTotalEnergy()
     momentum[i+1] = simulation.evaluateTotalMomentum()
